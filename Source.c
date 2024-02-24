04c9678f4db20ab4a70142b64ddada0a893b1a76
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2024-02-24 21:59:07 +0300

    ./

--- Source.c
+++ Source.c
@@ -4,6 +4,8 @@
 int main()
 {
 	float a = 0.0, b = 0.0, c = 0.0; //
+	char outputEquation[256];
+	char outputResult[256];
 	float x1, x2, d; //
 
 	puts("Input a, b, c:\n");
@@ -11,7 +13,8 @@ int main()
 	printf("b = "); scanf("%f", &b);
 	printf("c = "); scanf("%f", &c);
 
-	printf("Equation is:\n%.2f * x^2 + %.2f * x + %.2f = 0.00 \n", a, b, c);
+	sprintf(output, "Equation is:\n%g * x^2 + %g * x + %g = 0.00 \n", a, b, c);
+	puts(output);
 
 	if (a == 0)
 	{
@@ -28,7 +31,8 @@ int main()
 		else if (c == 0)
 		{
 			x1 = x2 = 0;
-			printf("x = %.2f\n", x1);
+			sprintf(outputResult, "x = %g\n", x1);
+			puts(outputResult);
 		}
 		else
 		{
@@ -43,13 +47,15 @@ int main()
 			if (b == 0)
 			{
 				x1 = x2 = 0;
-				printf("x = %.2f\n", x1);
+				sprintf(outputResult, "x = %g\n", x1);
+				puts(outputResult);
 			}
 			else
 			{
 				x1 = 0;
 				x2 = (float)(-1 * (b / a));
-				printf("x1 = %.2f\nx2 = %.2f\n", x1, x2);
+				sprintf(outputResult, "x1 = %g\nx2 = %g\n", x1, x2);
+				puts(outputResult);
 
 			}
 		}
@@ -58,8 +64,8 @@ int main()
 			if (b == 0 && ((a > 0 && c < 0) || (a < 0 && c > 0)))
 			{
 				x1 = x2 = (float)sqrt(-1 * (c / a));
-				printf("x = %.2f\n", x1);
-
+				sprintf(outputResult, "x = %g\n", x1);
+				puts(outputResult);
 			}
 			else
 			{
@@ -71,11 +77,13 @@ int main()
 
 					if (x1 == x2)
 					{
-						printf("x = %.2f\n", x1);
+						sprintf(outputResult, "x = %g\n", x1);
+						puts(outputResult);
 					}
 					else
 					{
-						printf("x1 = %.2f\nx2 = %.2f\n", x1, x2);
+						sprintf(outputResult,"x1 = %g\nx2 = %g\n", x1, x2);
+						puts(outputResult);
 					}
 				}
 				else
