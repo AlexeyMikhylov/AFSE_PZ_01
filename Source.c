335b8a231cd1ac228b0831e876ed192a2ae62760
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2024-02-25 09:21:19 +0300

    4.test/

--- Source.c
+++ Source.c
@@ -6,10 +6,29 @@ int main()
 	float a = 0.0, b = 0.0, c = 0.0; // коэффициенты при неизвестных
 	float x1, x2, D; // корни уравнения, дискриминант
 
-	puts("Input сoefficients of equation:\n"); //ввод коэффициентов с клавиатуры
-	printf("a = "); scanf("%f", &a);
+	puts("Input coefficients of equation:\n");
+	//ввод коэффициентов с клавиатуры
+	/*printf("a = "); scanf("%f", &a);
 	printf("b = "); scanf("%f", &b);
-	printf("c = "); scanf("%f", &c);
+	printf("c = "); scanf("%f", &c);*/
+
+	if (scanf("%f", &a) > 0)
+	{
+		printf("a = %g\n", a);
+	}
+	else printf("Not a number.\n");
+	
+	if (scanf("%f", &b) > 0)
+	{
+		printf("b = %g\n", b);
+	}
+	else printf("Not a number.\n");
+	
+	if (scanf("%f", &c) > 0)
+	{
+		printf("c = %g\n", c);
+	}
+	else printf("Not a number.\n");
 
 	//Вывод полученного уравнения
 	printf("Inputted equation is:\n%g * x^2 + %g * x + %g = 0.00 \n", a, b, c);
